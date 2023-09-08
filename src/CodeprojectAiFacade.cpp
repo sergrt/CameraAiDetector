9f2f76e94baa27081d6f185fc948e44e8c3ed5b6
sergrt <sergrt@fastmail.com>
2023-09-08 17:16:54 +0300

    Replace deprecated curl form with mime

--- src/CodeprojectAiFacade.cpp
+++ src/CodeprojectAiFacade.cpp
@@ -1,5 +1,6 @@
 #include "CodeprojectAiFacade.h"
 
+#include "FinalAction.h"
 #include "Log.h"
 
 #include <stdexcept>
@@ -34,6 +35,8 @@ CodeprojectAiFacade::~CodeprojectAiFacade() {
     curl_global_cleanup();
 }
 
+/*
+Old implementation - in case anything wrong with the new one
 nlohmann::json CodeprojectAiFacade::detect(const unsigned char* data, size_t data_size) {
     curl_easy_setopt(curl_, CURLOPT_URL, url_.c_str());
 
@@ -45,7 +48,7 @@ nlohmann::json CodeprojectAiFacade::detect(const unsigned char* data, size_t dat
         CURLFORM_BUFFER, "image",
         CURLFORM_BUFFERPTR, data,
         CURLFORM_BUFFERLENGTH, data_size,
-        CURLFORM_CONTENTTYPE, img_mime_type_.c_str(),  //"image/jpeg",
+        CURLFORM_CONTENTTYPE, img_mime_type_.c_str(),  // "image/jpeg", "image/bmp" etc.
         CURLFORM_END);
 
     curl_formadd(&form, &last,
@@ -62,6 +65,40 @@ nlohmann::json CodeprojectAiFacade::detect(const unsigned char* data, size_t dat
 
     const CURLcode res = curl_easy_perform(curl_);
     curl_formfree(form);
+
+    if (res == CURLE_OK) {
+        LogTrace() << "detect() ok, result: " << read_buffer;
+        return nlohmann::json::parse(read_buffer);
+    } else {
+        LogError() << "curl_easy_perform() failed: " << curl_easy_strerror(res);
+    }
+
+    return {};
+}
+*/
+
+nlohmann::json CodeprojectAiFacade::detect(const unsigned char* data, size_t data_size) {
+    curl_mime* mime = curl_mime_init(curl_);
+    auto _ = FinalAction([mime] { curl_mime_free(mime); });
+
+    curl_mimepart* image_part = curl_mime_addpart(mime);
+    curl_mime_data(image_part, reinterpret_cast<const char*>(data), data_size);
+    curl_mime_type(image_part, img_mime_type_.c_str());
+    curl_mime_name(image_part, "image");
+    
+    curl_mimepart* min_confidence_part = curl_mime_addpart(mime);
+    curl_mime_data(min_confidence_part, min_confidence_.c_str(), CURL_ZERO_TERMINATED);
+    curl_mime_type(min_confidence_part, "text/html");
+    curl_mime_name(min_confidence_part, "min_confidence");
+
+    curl_easy_setopt(curl_, CURLOPT_MIMEPOST, mime);
+    curl_easy_setopt(curl_, CURLOPT_URL, url_.c_str());
+
+    std::string read_buffer;
+    curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &read_buffer);
+    curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, writeCallback);
+
+    const CURLcode res = curl_easy_perform(curl_);
 
     if (res == CURLE_OK) {
         LogTrace() << "detect() ok, result: " << read_buffer;
