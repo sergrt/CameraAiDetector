f3d257cc52816818a7e3ff998df18f4d47d34e18
sergrt <sergrt@fastmail.com>
2023-10-13 19:31:42 +0300

    Fix json reading to throw exceptions on errors Use default values for some keys

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -32,30 +32,33 @@ Settings LoadSettings(const std::string& settings_file_name) {
         json = nlohmann::json::parse(stream);
     }
 
-    settings.source = json["source"];  // Mandatory options are obtained like this - throwable
-    settings.storage_path = json["storage_path"].get<std::string>();
+    settings.source = json.at("source");  // Mandatory options are obtained like this - throwable
+    settings.storage_path = json.at("storage_path").get<std::string>();
     settings.errors_before_reconnect = json.value("errors_before_reconnect", settings.errors_before_reconnect);
     settings.delay_after_error_ms = json.value("delay_after_error_ms", settings.delay_after_error_ms);
     settings.cooldown_write_time_ms = json.value("cooldown_write_time_ms", settings.cooldown_write_time_ms);
     settings.buffer_overflow_strategy = StringToBufferStrategy(json.value("buffer_overflow_strategy", "Delay"));
 
     settings.use_codeproject_ai = json.value("use_codeproject_ai", settings.use_codeproject_ai);
-    settings.codeproject_ai_url = json["codeproject_ai_url"];
-    settings.onnx_file_path = json["onnx_file_path"];
+    settings.codeproject_ai_url = json.value("codeproject_ai_url", settings.codeproject_ai_url);
+    settings.onnx_file_path = json.value("onnx_file_path", settings.onnx_file_path);
     settings.min_confidence = json.value("min_confidence", 0.4);
     settings.nth_detect_frame = json.value("nth_detect_frame", settings.nth_detect_frame);
     settings.use_image_scale = json.value("use_image_scale", settings.use_image_scale);
     settings.img_scale_x = json.value("img_scale_x", settings.img_scale_x);
     settings.img_scale_y = json.value("img_scale_y", settings.img_scale_y);
     settings.img_format = json.value("img_format", settings.img_format);
-    settings.frame_color = {json["frame_color"]["R"], json["frame_color"]["G"], json["frame_color"]["B"]};
+    if (json.contains("frame_color")) {
+        const auto color_json = json["frame_color"];
+        settings.frame_color = {color_json.at("R"), color_json.at("G"), color_json.at("B")};
+    }
     settings.frame_width_px = json.value("frame_width_px", settings.frame_width_px);
     settings.use_video_scale = json.value("use_video_scale", settings.use_video_scale);
     settings.video_width = json.value("video_width", settings.video_width);
     settings.video_height = json.value("video_height", settings.video_height );
 
-    settings.bot_token = json["bot_token"];
-    settings.allowed_users = json["allowed_users"].get<std::set<uint64_t>>();
+    settings.bot_token = json.at("bot_token");
+    settings.allowed_users = json.at("allowed_users").get<std::set<uint64_t>>();
     settings.alarm_notification_delay_ms = json.value("alarm_notification_delay_ms", settings.alarm_notification_delay_ms);
     settings.send_video_previews = json.value("send_video_previews", settings.send_video_previews);
     settings.send_video = json.value("send_video", settings.send_video);
