addf078d0705518bfd94dcc749a12a6574bae860
sergrt <sergrt@fastmail.com>
2023-12-17 12:21:01 +0300

    Comment out simple motion detection instrumentation

--- src/SimpleMotionDetect.cpp
+++ src/SimpleMotionDetect.cpp
@@ -14,7 +14,7 @@ SimpleMotionDetect::SimpleMotionDetect(const Settings::MotionDetectSettings& set
 }
 
 bool SimpleMotionDetect::Detect(const cv::Mat& image, std::vector<Detection>& detections) {
-    auto _ = instrument_detect_impl_.Trigger();
+    // auto _ = instrument_detect_impl_.Trigger();
     cv::Mat gray;
     cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
     cv::GaussianBlur(gray, gray, gaussian_sz_, 0);
