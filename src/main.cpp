f16f96afac8630dfe17e29ca3a100c3a558904eb
sergrt <sergrt@fastmail.com>
2026-03-03 23:56:21 +0300

    Rework main routine waiting for user signal to quit

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,7 @@
 
 #include <boost/program_options.hpp>
 
-#ifdef __linux__
-#include "unistd.h"
-#endif
-
+#include <atomic>
 #include <chrono>
 #include <string>
 
@@ -20,6 +17,14 @@ constexpr size_t kLogTailLines{32};
 SafePtr<RingBuffer<std::string>> AppLogTail{kLogTailLines};
 constexpr auto kSettingsFileName{"settings.json"};
 std::chrono::time_point<std::chrono::steady_clock> kStartTime{std::chrono::steady_clock::now()};
+
+std::atomic_bool keep_running{true};
+
+void SignalHandler(int signum) {
+    std::cout << "Caught signal " << signum << std::endl;
+    keep_running.store(false);
+    keep_running.notify_all();
+}
 
 int main(int argc, char* argv[]) {
     namespace po = boost::program_options;
@@ -41,7 +46,9 @@ int main(int argc, char* argv[]) {
     }
 
     std::ios_base::sync_with_stdio(false);
-    std::cout << "Application start, enter \"q\" to quit" << std::endl;
+
+    std::signal(SIGINT, SignalHandler);
+    std::cout << "Application start, press CTRL+C to quit" << std::endl;
 
     Settings settings;
     try {
@@ -68,32 +75,7 @@ int main(int argc, char* argv[]) {
     Core core(settings);
     core.Start();
 
-    std::string command;
-    while (true) {
-        const bool is_tty = 
-#ifdef __linux__
-            isatty(fileno(stdin));
-#else
-            true;
-#endif
-
-#ifdef __linux__
-        if (is_tty)
-            std::cin >> command;
-	else
-            std::this_thread::sleep_for(std::chrono::minutes(1));
-#else
-        std::cin >> command;
-#endif
-        if (!is_tty)
-            continue;
-
-        if (command == "q" || std::cin.fail() || std::cin.eof()) {
-            std::cout << "Exiting..." << std::endl;
-            core.Stop();
-            break;
-        } else {
-            std::cout << "Invalid command \"" << command << "\". Enter \"q\" to quit" << std::endl;
-        }
-    }
+    keep_running.wait(true);
+    std::cout << "Exiting..." << std::endl;
+    core.Stop();
 }
